c2113b48bc5e8f30f691c8d72c5a69eb2c08b95c
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-07-19 18:49:40 +0200

    feat: create a class

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import os
 from selenium import webdriver
 from dotenv import load_dotenv
+
+
+class InternetSpeedTwitterBot:
+    def __init__(self, driver_path):
+        self.driver = webdriver.Chrome(executable_path=driver_path)
+        self.up = 0
+        self.down = 0
+
+    def get_internet_speed(self):
+        pass
+
+    def tweet_at_provider(self):
+        pass
+
+
+bot = InternetSpeedTwitterBot(os.getenv("CHROME_DRIVER_PATH"))
+bot.get_internet_speed()
+bot.tweet_at_provider()
